a05ca3830c19e8e3a7b4401823abc26a025b17f2
Teja <teja992005@gmail.com>
2024-09-21 22:24:07 +0530

    updated

--- routes/listing.js
+++ routes/listing.js
@@ -7,7 +7,7 @@ const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/WrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const {listingSchema} = require("../schema.js");
-
+const {isLoggedIn} = require("../middleware.js");
 const validateListing = (req,res,next) =>{
     let {error} = listingSchema.validate(req.body);
 
@@ -26,7 +26,7 @@ router.get("/",wrapAsync(async (req,res) =>{
 }));
 
 //New Route
-router.get("/new",(req,res) =>{
+router.get("/new",isLoggedIn,(req,res) =>{
     res.render("listings/new.ejs");
 });
 
@@ -35,19 +35,23 @@ router.get("/new",(req,res) =>{
 router.get("/:id",wrapAsync(async (req,res) =>{
     let {id} = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if(!listing){
+        req.flash("error","Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
     res.render("listings/show.ejs",{listing});
 }));
 
 
 // Create Route
 
-router.post("/",validateListing,wrapAsync(async (req,res,next) =>{
+router.post("/",isLoggedIn,validateListing,wrapAsync(async (req,res,next) =>{
     
     // let {title,description,image,price,country,location} = req.body;
    
     const newListing = new Listing(req.body.listing);
-    
     await newListing.save();
+    req.flash("success","New Listing Created!");
     res.redirect("/listings");
       
       
@@ -55,26 +59,32 @@ router.post("/",validateListing,wrapAsync(async (req,res,next) =>{
    
    
    // Edit Route
-   router.get("/:id/edit",wrapAsync(async (req,res) =>{
+   router.get("/:id/edit",isLoggedIn,wrapAsync(async (req,res) =>{
        let {id} = req.params;
        const listing = await Listing.findById(id);
+       if(!listing){
+        req.flash("error","Listing you requested for does not exist!");
+        res.redirect("/listings");
+    }
        res.render("listings/edit.ejs",{listing});
    }));
    
    // Update Route
    
-   router.put("/:id",validateListing,wrapAsync(async (req,res) =>{
+   router.put("/:id",isLoggedIn,validateListing,wrapAsync(async (req,res) =>{
        let {id} = req.params;
        await Listing.findByIdAndUpdate(id,{...req.body.listing});
+       req.flash("success","Listing Updated!");
        res.redirect(`/listings/${id}`);
    }));
    
    
    //Delete Route
    
-   router.delete("/:id",wrapAsync(async (req,res) =>{
+   router.delete("/:id",isLoggedIn,wrapAsync(async (req,res) =>{
        let {id} = req.params;
        let deletedListing = await Listing.findByIdAndDelete(id);
+       req.flash("success","Listing deleted!");
        console.log(deletedListing);
        res.redirect("/listings");
    }));
@@ -92,4 +102,5 @@ router.post("/",validateListing,wrapAsync(async (req,res,next) =>{
        res.send("successuful testing");
    
    }));
+
 module.exports = router;
--- routes/review.js
+++ routes/review.js
@@ -30,6 +30,7 @@ router.post("/", validateReview,wrapAsync( async(req,res) =>{
     listing.reviews.push(newReview);
     await newReview.save();
     await listing.save();
+    req.flash("success","New Review Created!");
    res.redirect(`/listings/${listing._id}`);
 }));
 
@@ -38,6 +39,7 @@ router.delete("/:reviewId",wrapAsync(async(req,res) =>{
     let{id, reviewId} =  req.params;
     await Listing.findByIdAndUpdate(id,{$pull: {reviews: reviewId}});
     await Review.findByIdAndDelete(reviewId);
+    req.flash("success","Review deleted!");
     res.redirect(`/listings/${id}`);
 }));
 
